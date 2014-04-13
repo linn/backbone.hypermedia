0bc83face352d6216091fe396c63b18e1f096aed
Jim Liddell <liddell_j@hotmail.com>
2014-04-13 21:03:50 +0100

    Added more tests.

--- spec/Backbone.HypermediaModel.js
+++ spec/Backbone.HypermediaModel.js
@@ -36,14 +36,80 @@ define(function (require) {
             });
         });
 
-        describe('when the model has a link', function () {
+        describe('when sync is called', function () {
+            var options;
+
+            beforeEach(function () {
+                options = {};
+
+                spyOn(Backbone, 'sync').andCallFake(function () {
+                    var d = $.Deferred();
+                    d.resolve();
+                    return d.promise();
+                });
+
+                sut.sync('read', sut, options);
+            });
+
+            it('should call Backbone.sync', function () {
+                expect(Backbone.sync.argsForCall[0][1]).toBe(sut);
+            });
+        });
+
+        describe('when a mediaType property is defined', function () {
+            beforeEach(function () {
+                sut.mediaType = 'application/vnd.company.thing';
+            });
+
+            describe('and sync is called', function () {
+                var options;
+
+                beforeEach(function () {
+                    options = {};
+
+                    spyOn(Backbone, 'sync').andCallFake(function () {
+                        var d = $.Deferred();
+                        d.resolve();
+                        return d.promise();
+                    });
+
+                    sut.sync('read', sut, options);
+                });
+
+                it('should set the accepts option to the media type', function () {
+                    expect(options.accepts.json).toBe(sut.mediaType);
+                });
+            });
+
+            describe('and sync is called with method create', function () {
+                var options;
+
+                beforeEach(function () {
+                    options = {};
+
+                    spyOn(Backbone, 'sync').andCallFake(function () {
+                        var d = $.Deferred();
+                        d.resolve();
+                        return d.promise();
+                    });
+
+                    sut.sync('create', sut, options);
+                });
+
+                it('should set the contentType option to the media type', function () {
+                    expect(options.contentType).toBe(sut.mediaType);
+                });
+            });
+        });
+
+        describe('when the model declares a link', function () {
             beforeEach(function () {
                 sut.links = {
                     'some-rel': Backbone.Model
                 };
             });
 
-            describe('and the response contains a matching link', function () {
+            describe('and the response contains a corresponding link', function () {
                 beforeEach(function () {
                     spyOn(Backbone, 'sync').andCallFake(function (method, model, options) {
                         var d = $.Deferred();
@@ -90,8 +156,72 @@ define(function (require) {
                     it('should fire a follow event', function () {
                         expect(followFired).toBe(true);
                     });
+
+                    describe('and toJSON is called', function () {
+                        var json;
+
+                        beforeEach(function () {
+                            json = sut.toJSON();
+                        });
+
+                        it('should include the related model', function () {
+                            expect(json['some-rel']).toBeDefined();
+                        });
+                    });
                 });
             });
         });
+
+        describe('when the model declares a link within a nested scope', function () {
+            beforeEach(function () {
+                sut.links = {
+                    'scope.some-rel': Backbone.Model
+                };
+            });
+
+            describe('and the response contains a scope with a corresponding link', function () {
+                beforeEach(function () {
+                    spyOn(Backbone, 'sync').andCallFake(function (method, model, options) {
+                        var d = $.Deferred();
+                        var data = {
+                            scope: {
+                                links: [
+                                    { rel: 'some-rel', href: '/somewhere' }
+                                ]
+                            }
+                        };
+                        options.success(data);
+                        d.resolve(data);
+                        return d.promise();
+                    });
+                });
+
+                describe('and fetch is called', function () {
+                    var followFired = false;
+
+                    beforeEach(function () {
+                        sut.on('follow', function () {
+                            followFired = true;
+                        });
+                        sut.fetch();
+                    });
+
+                    it('should add the related model to the nested scope', function () {
+                        expect(sut.get('scope')['some-rel']).toBeDefined();
+                        expect(sut.get('scope')['some-rel'] instanceof Backbone.Model).toBeTruthy();
+                    });
+                });
+            });
+
+            // and the response contains an array with a corresponding link in each item
+        });
+
+        // when the model defines a link against a nested Backbone model
+
+        // when the model defined a link against a nested Backbone collection
+
+        // when the model declares the link as allowing multiple occurences
+
+        // when multiple occurences of a link are encountered where only one was expected
     });
 });
