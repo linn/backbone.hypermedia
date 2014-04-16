a3d68e5d80f098c5f801b87323288a2b0e7034e5
Jim Liddell <liddell_j@hotmail.com>
2014-04-16 21:54:34 +0100

    Added publish task for publishing to npm and nuget (fixes #11).

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,15 +53,43 @@ module.exports = function (grunt) {
 
         jshint: {
             all: ['Gruntfile.js', 'src/**/*.js', 'spec/**/*.js']
+        },
+
+        clean: ['nuget/*.nupkg'],
+
+        release: {
+            options: {
+                bump: false, // use grunt-bump for this as it can reload pkg
+                add: false,
+                commit: false,
+                tag: false,
+                push: false,
+                pushTags: false
+            }
+        },
+
+        bump: {
+            options: {
+                updateConfigs: ['pkg'],
+                commit: false,
+                createTag: false,
+                push: false
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-bump');
     grunt.loadNpmTasks('grunt-nuget');
+    grunt.loadNpmTasks('grunt-release');
 
     grunt.registerTask('test', ['jshint', 'jasmine']);
-    grunt.registerTask('default', ['jshint', 'jasmine']);
-
+    grunt.registerTask('default', ['test']);
+    grunt.registerTask('publish', ['publish:patch']);
+    grunt.registerTask('publish:patch', ['clean', 'bump', 'release:patch', 'nugetpack', 'nugetpush']);
+    grunt.registerTask('publish:minor', ['clean', 'bump', 'release:minor', 'nugetpack', 'nugetpush']);
+    grunt.registerTask('publish:major', ['clean', 'bump', 'release:major', 'nugetpack', 'nugetpush']);
 };
